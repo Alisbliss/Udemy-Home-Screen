211d14e87d6b66cdfdcfe52614c4cbd71efda5d5
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2026-01-24 17:24:16 +0300

    Initial Commit

---
+++ ios-udemy-home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-udemy-home
+//
+//  Created by Алеся Афанасенкова on 24.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
