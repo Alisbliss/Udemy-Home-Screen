3383670ff4a224613f0a117165806231b29d2e35
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2026-01-24 19:07:53 +0300

    Add UIModel

--- ios-udemy-home/Model/UIHomeModel.swift
+++ ios-udemy-home/Model/UIHomeModel.swift
@@ -5,3 +5,30 @@
 //  Created by Алеся Афанасенкова on 24.01.2026.
 //
 
+import Foundation
+
+struct HomeUIModel: Hashable {
+    
+    struct SectionModel: Hashable {
+        let section: Section
+        let body:  [Item]
+    }
+    
+    enum Section: Hashable {
+        case mainBanner(id: String)
+        case textHeader(id: String)
+        case courseSwimLine(id: String)
+        case udemyBusinessBanner(id: String)
+        case categories(id: String)
+        case faturedCourse(id: String)
+    }
+    
+    enum Item: Hashable {
+        case mainBanner(id: String, imageLink: String, title: String, caption: String)
+        case course(id: String, imageLink: String, title: String, author: String, rating: Double, reviewCount: Int, price: Decimal, tag: String)
+        case textHeader(id: String, text: String, highlightedText: String?)
+        case udemyBusinessBanner(id: String, link: String)
+        case categoriesScroller(id: String, titles: [String])
+        case featuredCourse(id: String, imageLink: String, title: String, author: String, rating: Double, reviewCount: Int, price: Decimal)
+    }
+}
