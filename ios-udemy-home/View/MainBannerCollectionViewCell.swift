aa667131fa9d14bf3f11456ff97e1ed655ca36df
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2026-02-07 23:27:54 +0300

    Add MainBannerCollectionViewCell

--- ios-udemy-home/View/MainBannerCollectionViewCell.swift
+++ ios-udemy-home/View/MainBannerCollectionViewCell.swift
@@ -5,3 +5,29 @@
 //  Created by Алеся Афанасенкова on 24.01.2026.
 //
 
+import SwiftUI
+import SnapKit
+
+final class MainBannerCollectionViewCell: UICollectionViewCell {
+    
+    private var hostingController: UIHostingController<MainBannerView>!
+    
+    func cofigure(
+        imageLink: String,
+        title: String,
+        caption: String
+    ) {
+        guard hostingController == nil else { return }
+        let mainBannerView = MainBannerView(
+            ImageLink: imageLink,
+            title: title,
+            caption: caption)
+        hostingController = UIHostingController(rootView: mainBannerView)
+        guard let hostingController = hostingController else { return }
+        addSubview(hostingController.view)
+        hostingController.view.clipsToBounds = true
+        hostingController.view.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
