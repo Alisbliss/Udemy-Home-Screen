a65ace21c22960fa1e93a43d6059b9e9acd98bba
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2026-02-10 13:35:42 +0300

    Added the link for fetching data

--- ios-udemy-home/Helper/HomeUIModelHelper.swift
+++ ios-udemy-home/Helper/HomeUIModelHelper.swift
@@ -5,3 +5,49 @@
 //  Created by Алеся Афанасенкова on 24.01.2026.
 //
 
+import Foundation
+
+struct HomeUIModelHelper {
+    static func makeUIModel(response: APIResponse) -> HomeUIModel {
+        typealias SectionModel = HomeUIModel.SectionModel
+        var sectionModels = [SectionModel]()
+        for layout in response.layouts {
+            switch layout {
+            case let .mainBanner(id, mainBanner):
+                let sectionModel = SectionModel(section: .mainBanner(id: id), body: [
+                    .mainBanner(id: mainBanner.id, imageLink: mainBanner.imageLink, title: mainBanner.title, caption: mainBanner.caption)
+                ])
+                sectionModels.append(sectionModel)
+            case let .textHeader(id, textHeader):
+                let sectionModel = SectionModel(section: .textHeader(id: id), body: [
+                    .textHeader(id: textHeader.id, text: textHeader.text, highlightedText: textHeader.highlightedText)
+                ])
+                sectionModels.append(sectionModel)
+            case let .courseSwimlane(id, courses):
+                let items: [HomeUIModel.Item] = courses.map { cource in
+                    return .course(id: cource.id, imageLink: cource.imageLink, title: cource.title, author: cource.author, rating: cource.rating, reviewCount: cource.reviewCount, price: cource.price, tag: cource.tag)
+                }
+                let sectionModel = SectionModel(section: .courseSwimLine(id: id), body: items)
+                sectionModels.append(sectionModel)
+            case let .categories(id, categories):
+                let sectionModel = SectionModel(section: .categories(id: id), body: [
+                    .categoriesScroller(id: categories.id, titles: categories.titles)
+                ])
+                sectionModels.append(sectionModel)
+            case let .featuredCourse(id, course):
+                let sectionModel = SectionModel(section: .faturedCourse(id: id), body: [
+                    .featuredCourse(id: course.id, imageLink: course.imageLink, title: course.title, author: course.author, rating: course.rating, reviewCount: course.reviewCount, price: course.price)])
+                sectionModels.append(sectionModel)
+                print(sectionModel)
+            case let .udemyBusinessBanner(id, udemyBusinessBanner):
+                let sectionModel = SectionModel(section: .udemyBusinessBanner(id: id), body: [
+                    .udemyBusinessBanner(id: udemyBusinessBanner.id, link: udemyBusinessBanner.link)
+                ])
+                sectionModels.append(sectionModel)
+            case .unknown(let type):
+                print(">>>>> unknown type found \(type)")
+            }
+        }
+        return HomeUIModel(sectionModels: sectionModels)
+    }
+}
--- ios-udemy-home/Home/HomeViewController.swift
+++ ios-udemy-home/Home/HomeViewController.swift
@@ -12,142 +12,143 @@ import SafariServices
 
 class HomeViewController: UIViewController {
 
+    private let apiClient = APIClient()
     private let collectionView = HomeCollectionView()
     private var cancellable = Set<AnyCancellable>()
     
     override func loadView() {
         super.loadView()
         observe()
-        loadJSON()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchLayout()
         setupView()
-        let uiModel = HomeUIModel(sectionModels: [
-            .init(section: .mainBanner(id: "123"), body: [
-                .mainBanner(
-                    id: "123",
-                    imageLink: "https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    title: "Learning that fits",
-                    caption: "Skills for your present and future")
-            ]),
-            .init(section: .textHeader(id: "2321"), body: [
-                .textHeader(
-                    id: "232",
-                    text: "Newest cources in Mobile Development",
-                    highlightedText: "Mobile Development")
-            ]),
-            .init(section: .courseSwimLine(id: "234"), body: [
-                .course(
-                    id: "12395",
-                    imageLink: "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
-                    title: "iOS & Swift: Server Driven UI Compositional Layout & SwiftUI",
-                    author: "Kelvin Fok",
-                    rating: 4.5,
-                    reviewCount: 4569,
-                    price: 19.99,
-                    tag: "bestsellar"),
-                .course(
-                    id: "12348",
-                    imageLink: "https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29kaW5nfGVufDB8fDB8fHww",
-                    title: "iOS & Swift: Swift UI Mastery",
-                    author: "Kelvin Fok",
-                    rating: 5.0,
-                    reviewCount: 69,
-                    price: 19.99,
-                    tag: "bestsellar"),
-                .course(
-                    id: "12345",
-                    imageLink: "https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
-                    title: "iOS & Swift: Auto Layout",
-                    author: "Kelvin Fok",
-                    rating: 4.7,
-                    reviewCount: 459,
-                    price: 19.99,
-                    tag: "bestsellar")
-            ]),
-            .init(section: .textHeader(id: "656756"), body: [
-                .textHeader(id: "244556", text: "Categories", highlightedText: nil)
-            ]),
-            .init(section: .categories(id: "4567977"), body: [
-                .categoriesScroller(id: "6565", titles: Category.allCases.map({ $0.rawValue.camelCaseToEnglish.useShortAndFormat.capitalized
-                }))
-            ]),
-            .init(section: .textHeader(id: "2328761"), body: [
-                .textHeader(
-                    id: "232766",
-                    text: "Because you viewed \"How to land yourself a role\"",
-                    highlightedText: "How to land yourself a role")
-            ]),
-            .init(section: .courseSwimLine(id: "276634"), body: [
-                .course(
-                    id: "12384695",
-                    imageLink: "https://media.istockphoto.com/id/2233679888/photo/young-programmer-analyzing-code-on-laptop-and-large-screen-in-modern-office.webp?a=1&b=1&s=612x612&w=0&k=20&c=nsCFTjtILnzpSZFkHrUUt8cFPoczZW9XgXCPsS3ifiQ=",
-                    title: "iOS & Swift: Server Driven UI Compositional Layout & SwiftUI",
-                    author: "Kelvin Fok",
-                    rating: 4.5,
-                    reviewCount: 4569,
-                    price: 19.99,
-                    tag: "bestsellar"),
-                .course(
-                    id: "1236788",
-                    imageLink: "https://media.istockphoto.com/id/1530973530/photo/software-development-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=NXxmootfVkI2C_JS5-5p06qMD_ngxJnH8BfLxnoQKP8=",
-                    title: "iOS & Swift: Swift UI Mastery",
-                    author: "Kelvin Fok",
-                    rating: 5.0,
-                    reviewCount: 69,
-                    price: 19.99,
-                    tag: "bestsellar"),
-                .course(
-                    id: "12348905",
-                    imageLink: "https://media.istockphoto.com/id/2219298648/photo/code-of-conduct-as-central-puzzle-piece.webp?a=1&b=1&s=612x612&w=0&k=20&c=ARKzV2SfHLXUH1GI78WbHiJsbgwGhL-D6--B9GiEnX0=",
-                    title: "iOS & Swift: Auto Layout",
-                    author: "Kelvin Fok",
-                    rating: 4.7,
-                    reviewCount: 459,
-                    price: 19.99,
-                    tag: "bestsellar"),
-                .course(
-                    id: "12384695",
-                    imageLink: "https://media.istockphoto.com/id/2222660797/photo/global-data-flow.webp?a=1&b=1&s=612x612&w=0&k=20&c=MC5B4KJI9SlfpE1SlcmYzNIB530O56HfhlaNeYuvhzo=",
-                    title: "iOS & Swift: Server Driven UI Compositional Layout & SwiftUI",
-                    author: "Kelvin Fok",
-                    rating: 4.5,
-                    reviewCount: 4569,
-                    price: 19.99,
-                    tag: "bestsellar"),
-                .course(
-                    id: "1236788",
-                    imageLink: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
-                    title: "iOS & Swift: Swift UI Mastery",
-                    author: "Kelvin Fok",
-                    rating: 5.0,
-                    reviewCount: 69,
-                    price: 19.99,
-                    tag: "bestsellar")
-            ]),
-            .init(section: .textHeader(id: "689999999"), body: [
-                .textHeader(
-                    id: "76567575",
-                    text: "Top course of the year",
-                    highlightedText: nil)
-            ]),
-            .init(section: .faturedCourse(id: "65677667"), body:  [
-                .featuredCourse(
-                    id: "543667778",
-                    imageLink: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
-                    title: "iOS & Swift: Swift UI Mastery",
-                    author: "Kelvin Fok",
-                    rating: 5.0,
-                    reviewCount: 69,
-                    price: 19.99)
-            ]),
-            .init(section: .udemyBusinessBanner(id: "7656576565656"), body: [.udemyBusinessBanner(id: "76576565675", link: "https://www.udemy.com")
-                
-            ])
-        ])
-        collectionView.setDataSource(uiModel: uiModel)
+//        let uiModel = HomeUIModel(sectionModels: [
+//            .init(section: .mainBanner(id: "123"), body: [
+//                .mainBanner(
+//                    id: "123",
+//                    imageLink: "https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//                    title: "Learning that fits",
+//                    caption: "Skills for your present and future")
+//            ]),
+//            .init(section: .textHeader(id: "2321"), body: [
+//                .textHeader(
+//                    id: "232",
+//                    text: "Newest cources in Mobile Development",
+//                    highlightedText: "Mobile Development")
+//            ]),
+//            .init(section: .courseSwimLine(id: "234"), body: [
+//                .course(
+//                    id: "12395",
+//                    imageLink: "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+//                    title: "iOS & Swift: Server Driven UI Compositional Layout & SwiftUI",
+//                    author: "Kelvin Fok",
+//                    rating: 4.5,
+//                    reviewCount: 4569,
+//                    price: 19.99,
+//                    tag: "bestsellar"),
+//                .course(
+//                    id: "12348",
+//                    imageLink: "https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29kaW5nfGVufDB8fDB8fHww",
+//                    title: "iOS & Swift: Swift UI Mastery",
+//                    author: "Kelvin Fok",
+//                    rating: 5.0,
+//                    reviewCount: 69,
+//                    price: 19.99,
+//                    tag: "bestsellar"),
+//                .course(
+//                    id: "12345",
+//                    imageLink: "https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+//                    title: "iOS & Swift: Auto Layout",
+//                    author: "Kelvin Fok",
+//                    rating: 4.7,
+//                    reviewCount: 459,
+//                    price: 19.99,
+//                    tag: "bestsellar")
+//            ]),
+//            .init(section: .textHeader(id: "656756"), body: [
+//                .textHeader(id: "244556", text: "Categories", highlightedText: nil)
+//            ]),
+//            .init(section: .categories(id: "4567977"), body: [
+//                .categoriesScroller(id: "6565", titles: Category.allCases.map({ $0.rawValue.camelCaseToEnglish.useShortAndFormat.capitalized
+//                }))
+//            ]),
+//            .init(section: .textHeader(id: "2328761"), body: [
+//                .textHeader(
+//                    id: "232766",
+//                    text: "Because you viewed \"How to land yourself a role\"",
+//                    highlightedText: "How to land yourself a role")
+//            ]),
+//            .init(section: .courseSwimLine(id: "276634"), body: [
+//                .course(
+//                    id: "12384695",
+//                    imageLink: "https://media.istockphoto.com/id/2233679888/photo/young-programmer-analyzing-code-on-laptop-and-large-screen-in-modern-office.webp?a=1&b=1&s=612x612&w=0&k=20&c=nsCFTjtILnzpSZFkHrUUt8cFPoczZW9XgXCPsS3ifiQ=",
+//                    title: "iOS & Swift: Server Driven UI Compositional Layout & SwiftUI",
+//                    author: "Kelvin Fok",
+//                    rating: 4.5,
+//                    reviewCount: 4569,
+//                    price: 19.99,
+//                    tag: "bestsellar"),
+//                .course(
+//                    id: "1236788",
+//                    imageLink: "https://media.istockphoto.com/id/1530973530/photo/software-development-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=NXxmootfVkI2C_JS5-5p06qMD_ngxJnH8BfLxnoQKP8=",
+//                    title: "iOS & Swift: Swift UI Mastery",
+//                    author: "Kelvin Fok",
+//                    rating: 5.0,
+//                    reviewCount: 69,
+//                    price: 19.99,
+//                    tag: "bestsellar"),
+//                .course(
+//                    id: "12348905",
+//                    imageLink: "https://media.istockphoto.com/id/2219298648/photo/code-of-conduct-as-central-puzzle-piece.webp?a=1&b=1&s=612x612&w=0&k=20&c=ARKzV2SfHLXUH1GI78WbHiJsbgwGhL-D6--B9GiEnX0=",
+//                    title: "iOS & Swift: Auto Layout",
+//                    author: "Kelvin Fok",
+//                    rating: 4.7,
+//                    reviewCount: 459,
+//                    price: 19.99,
+//                    tag: "bestsellar"),
+//                .course(
+//                    id: "12384695",
+//                    imageLink: "https://media.istockphoto.com/id/2222660797/photo/global-data-flow.webp?a=1&b=1&s=612x612&w=0&k=20&c=MC5B4KJI9SlfpE1SlcmYzNIB530O56HfhlaNeYuvhzo=",
+//                    title: "iOS & Swift: Server Driven UI Compositional Layout & SwiftUI",
+//                    author: "Kelvin Fok",
+//                    rating: 4.5,
+//                    reviewCount: 4569,
+//                    price: 19.99,
+//                    tag: "bestsellar"),
+//                .course(
+//                    id: "1236788",
+//                    imageLink: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+//                    title: "iOS & Swift: Swift UI Mastery",
+//                    author: "Kelvin Fok",
+//                    rating: 5.0,
+//                    reviewCount: 69,
+//                    price: 19.99,
+//                    tag: "bestsellar")
+//            ]),
+//            .init(section: .textHeader(id: "689999999"), body: [
+//                .textHeader(
+//                    id: "76567575",
+//                    text: "Top course of the year",
+//                    highlightedText: nil)
+//            ]),
+//            .init(section: .faturedCourse(id: "65677667"), body:  [
+//                .featuredCourse(
+//                    id: "543667778",
+//                    imageLink: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+//                    title: "iOS & Swift: Swift UI Mastery",
+//                    author: "Kelvin Fok",
+//                    rating: 5.0,
+//                    reviewCount: 69,
+//                    price: 19.99)
+//            ]),
+//            .init(section: .udemyBusinessBanner(id: "7656576565656"), body: [.udemyBusinessBanner(id: "76576565675", link: "https://www.udemy.com")
+//                
+//            ])
+//        ])
+//        collectionView.setDataSource(uiModel: uiModel)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -206,9 +207,37 @@ class HomeViewController: UIViewController {
         navigationController?.present(safaryViewController, animated: true)
     }
     
-    private func loadJSON() {
-        let response: APIResponse? = FileManager.modelFromJSON(fileName: "Payload")
-        print(" Response: \(response)")
+    private func fetchLayout() {
+        
+        apiClient.fetchLayout()
+            .receive(on: DispatchQueue.main)
+            .sink
+            { completion in
+            if case let .failure(error) = completion {
+                print(error)
+            }
+        } receiveValue: { [weak self] apiPesponse in
+            let uiModel = HomeUIModelHelper.makeUIModel(response: apiPesponse)
+            self?.collectionView.setDataSource(uiModel: uiModel)
+        }.store(in: &cancellable)
+        
+//        let apiResponse: APIResponse? = FileManager.modelFromJSON(fileName: "Payload")
+//        if let response = apiResponse {
+//            let uiModel = HomeUIModelHelper.makeUIModel(response: response)
+//            print ("UIModeL: \(uiModel)")
+//            collectionView.setDataSource(uiModel: uiModel)
+//        }
+        //print(" Response: \(apiresponse)")
     }
 }
 
+struct APIClient {
+    private let urlString = "https://mocki.io/v1/dcbfb0e8-1ddb-4606-bb67-393617e26fc7"
+    func fetchLayout() -> AnyPublisher<APIResponse, Error> {
+        let url = URL(string: urlString)!
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map({ $0.data})
+            .decode(type: APIResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
